aa7bbdeb96bf49d50ff4349fcb157ad8a528f8fa
Hamza Maach <hamzamaach56@gmail.com>
2023-01-23 13:00:50 +0100

    first commit

---
+++ bootstrap.php
@@ -0,0 +1,5 @@
+<?php
+
+    define('base','http://localhost/MVC/')
+
+?>
---
+++ views/home.php
@@ -0,0 +1,8 @@
+<body>
+<div class="d-flex align-items-center justify-content-center vh-100">
+    <div class="text-center">
+        <h1 class="display-1 fw-bold">Home</h1>
+        <p class="fs-3"> <span class="text-danger">Bonjour !</span></p>
+    </div>
+</div>
+</body>
